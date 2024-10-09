62714cec03909afc6d1b86c7c710229dc18e81c4
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-10-09 21:30:51 +0000

    refact: add image_url to products model

--- products/models.py
+++ products/models.py
@@ -27,7 +27,8 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     sku = models.CharField(max_length=100, null=True, blank=True)
-    image = models.ImageField(upload_to='media/', blank=True, null=True)  # For uploaded images
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
