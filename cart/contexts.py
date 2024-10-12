ead982347e4030074562c452da9ca8b89c3eaa05
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-10-12 06:17:06 +0000

    add: cart total update using context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,13 +1,27 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def cart_contents(request):
+
+    cart = request.session.get('cart', {})
 
     cart_items = []
 
     total = 0
 
     product_count = 0
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += product.price * quantity
+        cart_items.append({
+            'product': product,
+            'quantity': quantity,
+            'item_id': item_id
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         # Case 1: Apply delivery charge, calculate both differences
--- cart/views.py
+++ cart/views.py
@@ -27,30 +27,15 @@ def add_to_cart(request, item_id):
         if item_id in list(cart.keys()):
             if size in cart[item_id]['items_by_size'].keys():
                 cart[item_id]['items_by_size'][size] += quantity
-                messages.success(request,
-                                 (f'Updated size {size.upper()} '
-                                  f'{product.name} quantity to '
-                                  f'{cart[item_id]["items_by_size"][size]}'))
             else:
                 cart[item_id]['items_by_size'][size] = quantity
-                messages.success(request,
-                                 (f'Added size {size.upper()} '
-                                  f'{product.name} to your cart'))
         else:
             cart[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request,
-                             (f'Added size {size.upper()} '
-                              f'{product.name} to your cart'))
     else:
         if item_id in list(cart.keys()):
             cart[item_id] += quantity
-            messages.success(request,
-                             (f'Updated {product.name} '
-                              f'quantity to {cart[item_id]}'))
         else:
             cart[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your cart')
 
     request.session['cart'] = cart
-    print(request.session['cart'])
     return redirect(redirect_url)
