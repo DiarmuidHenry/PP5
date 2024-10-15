c9bdde850ecd5a357c5f5395a2731641338c0b14
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-10-15 19:25:23 +0000

    refact: change filters, update url links to account pages

--- products/views.py
+++ products/views.py
@@ -24,7 +24,9 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-
+            if sortkey == 'rating':
+                products = products.annotate(average_rating=Avg('ratings__rating'))
+                sortkey = 'average_rating'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
