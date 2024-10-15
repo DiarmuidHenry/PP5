c34e9a6b91ac593c6a7015d30601b8addb1c4213
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-10-15 22:23:10 +0000

    add: display previous order details. Images not being found

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
-    path('my_orders', views.my_orders, name='my_orders' )
+    path('my_orders', views.my_orders, name='my_orders'),
+    path('my_orders/<str:order_number>/', views.order_details, name='order_details'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from checkout.models import Order 
+from checkout.models import Order
+from products.models import Product
+from django.conf import settings
 
 
 def index(request):
@@ -19,3 +21,40 @@ def my_orders(request):
         orders = []  # No orders if user is not authenticated
 
     return render(request, 'home/my_orders.html', {'orders': orders})
+
+
+@login_required
+@login_required
+def order_details(request, order_number):
+    order = get_object_or_404(Order, order_number=order_number)
+
+    # Prepare a list of product details for each item in order_items
+    product_details = []
+    for item in order.order_items:  
+        product = get_object_or_404(Product, id=item['item_id']) 
+        
+        # Create a new dictionary with the desired details
+        product_info = {
+            'description': product.description,
+            'co2_footårint': product.co2_footprint,
+            'option': item['option'],
+            'image_url': product.image_url if product.image else '/media/no_image.jpg',
+            'item_total': item['price'],  
+            'name': product.name,  
+            'price': product.price,  
+            'quantity': item['quantity'],  
+        }
+        product_details.append(product_info)  # Append to the list
+
+    context = {
+        'order': order,
+        'product_details': product_details,  # Pass the new list of product details
+        'free_tote_bag_threshold': settings.FREE_TOTE_BAG_THRESHOLD,
+        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+    }
+
+    print("context:")
+    print(context)  # Debugging: print the context to verify
+
+    return render(request, 'home/order_details.html', context)
+
