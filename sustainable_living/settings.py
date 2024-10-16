e8110f2dae5ccecf88b88aacdc062250550febb7
Diarmuid Henry <diarmuidhenry@live.co.uk>
2024-10-16 21:38:46 +0000

    deploy

--- sustainable_living/settings.py
+++ sustainable_living/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 if os.path.isfile('env.py'):
     import env
 
@@ -29,7 +30,9 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    '8000-diarmuidhenry-pp5-02wc260itsh.ws.codeinstitute-ide.net'
+    '8000-diarmuidhenry-pp5-02wc260itsh.ws.codeinstitute-ide.net',
+    '.herokuapp.com',
+    'localhost',
     ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -124,12 +127,17 @@ WSGI_APPLICATION = 'sustainable_living.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
